ab081f47541e08b64ede4120a332afb5bd3af37f
RachelTurekSullivan <Rachel.Sullivan@improving.com>
2021-07-12 08:23:11 -0500

    Add project files.

---
+++ Fancy Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fancy_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1;
+            string input2;
+            float sum;
+            float num1;
+            float num2;
+
+
+            Console.WriteLine("A Console Calculator");
+            Console.WriteLine("Enter a number: ");
+            input1 = Console.ReadLine();
+            Console.WriteLine("Enter a second number, and I will add it to the first: ");
+            input2 = Console.ReadLine();
+            sum = float.Parse(input1) + float.Parse(input2);
+            Console.WriteLine("Result: " + sum);
+        }
+    }
+}
