5ed92c399a5b1567275dd3fae05012cf5f0b30a2
RachelTurekSullivan <Rachel.Sullivan@improving.com>
2021-07-12 08:47:33 -0500

    Step 2 - input verificaton

---
+++ Fancy Calculator/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fancy_Calculator
+{
+    public class DataService
+    {
+        public float GetInput()
+        {
+
+            string input = Console.ReadLine();
+            float verifiedInput;
+
+            while (VerifyNumericalInput(input) == false)
+            {
+                Console.WriteLine("The value " + input + " was not a valid number. Please enter a number: ");
+                input = Console.ReadLine();
+            }
+
+            verifiedInput = float.Parse(input);
+
+            return verifiedInput;
+
+        }
+        public bool VerifyNumericalInput(string input)
+        {
+            if (input.Equals('0'))
+            {
+                return true;
+            }
+            else 
+            { 
+                var isNumber = float.TryParse(input, out _);
+                if (isNumber == true)
+                {
+                    return true;
+                }
+                else { return false; }
+            }
+        }
+
+    }
+}
--- Fancy Calculator/Program.cs
+++ Fancy Calculator/Program.cs
@@ -4,21 +4,25 @@ namespace Fancy_Calculator
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            string input1;
-            string input2;
+
             float sum;
             float num1;
             float num2;
-
+            DataService dataService = new DataService();
 
             Console.WriteLine("A Console Calculator");
             Console.WriteLine("Enter a number: ");
-            input1 = Console.ReadLine();
+
+           num1 = dataService.GetInput();
+
             Console.WriteLine("Enter a second number, and I will add it to the first: ");
-            input2 = Console.ReadLine();
-            sum = float.Parse(input1) + float.Parse(input2);
+
+            num2 = dataService.GetInput();
+
+            sum = num1+num2;
             Console.WriteLine("Result: " + sum);
         }
     }
